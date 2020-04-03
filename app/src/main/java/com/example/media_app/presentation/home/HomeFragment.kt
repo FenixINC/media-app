311458f96b2d614a025c97a62e5df57444d21a2a
taras_koloshmatin <taras.koloshmatin@gmail.com>
2020-04-04 01:24:53 +0300

    two way binding

--- app/src/main/java/com/example/media_app/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/media_app/presentation/home/HomeFragment.kt
@@ -1,35 +1,34 @@
 package com.example.media_app.presentation.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.media_app.R
-import com.example.media_app.data.network.graphql.GraphQlGenerator
+import com.example.media_app.databinding.FragmentHomeBinding
 import com.example.media_app.presentation.base.BaseFragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.koin.android.ext.android.inject
 
 class HomeFragment : BaseFragment<HomeViewModel>() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override val viewModel by inject<HomeViewModel>()
 
     override fun getLayoutRes() = R.layout.fragment_home
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        val clientApollo = GraphQlGenerator.apolloClient
-
-        CoroutineScope(Dispatchers.Main).launch {
-            withContext(Dispatchers.IO) {
-//                clientApollo.query(GetCharacterListQuery)
-            }
-        }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-//    override fun onBackPressed() {
-//        super.onBackPressed()
-//        activity?.finish()
-//    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.viewModel = viewModel
+
+        // Two way binding
+        binding.btnClick.setOnClickListener {
+            viewModel.someText.set(binding.editText.text.toString())
+        }
+    }
 }
--- app/src/main/java/com/example/media_app/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/media_app/presentation/home/HomeViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.media_app.presentation.home
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 import com.example.media_app.presentation.base.BaseViewModel
 
 class HomeViewModel : BaseViewModel() {
+
+    val textLiveData = MutableLiveData<String>()
+
+    var someText = ObservableField<String>()
+
+    fun changeText(text: String) {
+        textLiveData.postValue(text)
+    }
+
+    fun updateTextResult() {
+
+    }
 }
