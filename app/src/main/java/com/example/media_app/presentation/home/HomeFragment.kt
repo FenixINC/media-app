e3c799d50356a8c95fb408c0fc8560e20c469959
taras_koloshmatin <taras.koloshmatin@gmail.com>
2020-04-12 10:40:02 +0300

    clean up

--- app/src/main/java/com/example/media_app/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/media_app/presentation/home/HomeFragment.kt
@@ -1,13 +1,9 @@
 package com.example.media_app.presentation.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.media_app.R
-import com.example.media_app.databinding.FragmentHomeBinding
 import com.example.media_app.presentation.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.serialization.ImplicitReflectionSerializer
@@ -15,22 +11,15 @@ import org.koin.android.ext.android.inject
 
 class HomeFragment : BaseFragment<HomeViewModel>() {
 
-    private lateinit var binding: FragmentHomeBinding
     private lateinit var characterAdapter: CharactersAdapter
 
     override val viewModel by inject<HomeViewModel>()
 
     override fun getLayoutRes() = R.layout.fragment_home
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
     @ImplicitReflectionSerializer
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        binding.viewModel = viewModel
 
         setAdapter()
 
